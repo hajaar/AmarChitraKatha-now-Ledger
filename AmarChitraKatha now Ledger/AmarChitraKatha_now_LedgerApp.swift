7e235f12708a52fd1350ee7b8011fec033f5d5e5
Kartik Narayanan <hajaar@icloud.com>
2022-04-24 10:18:03 +0530

    Initial Commit

---
+++ AmarChitraKatha now Ledger/AmarChitraKatha_now_LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmarChitraKatha_now_LedgerApp.swift
+//  AmarChitraKatha now Ledger
+//
+//  Created by Kartik Narayanan on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AmarChitraKatha_now_LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
